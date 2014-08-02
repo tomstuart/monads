3ce52a5d31fd3a60cea61121963a6194ee827b51
Tom Stuart <tom@codon.com>
2014-08-02 12:04:55 +0200

    Implement Optional#and_then

--- lib/monads/optional.rb
+++ lib/monads/optional.rb
@@ -1,3 +1,11 @@
 module Monads
-  Optional = Struct.new(:value)
+  Optional = Struct.new(:value) do
+    def and_then(&block)
+      if value.nil?
+        self
+      else
+        block.call(value)
+      end
+    end
+  end
 end
--- spec/monads/optional_spec.rb
+++ spec/monads/optional_spec.rb
@@ -10,5 +10,34 @@ module Monads
         expect(optional.value).to eq value
       end
     end
+
+    describe '#and_then' do
+      context 'when the value is nil' do
+        before(:example) do
+          allow(value).to receive(:nil?).and_return(true)
+        end
+
+        it 'doesn’t call the block' do
+          expect { |block| optional.and_then(&block) }.not_to yield_control
+        end
+      end
+
+      context 'when the value isn’t nil' do
+        before(:example) do
+          allow(value).to receive(:nil?).and_return(false)
+        end
+
+        it 'calls the block with the value' do
+          @value = nil
+          optional.and_then { |value| @value = value }
+          expect(@value).to eq value
+        end
+
+        it 'returns the block’s result' do
+          result = double
+          expect(optional.and_then { |value| Optional.new(result) }.value).to eq result
+        end
+      end
+    end
   end
 end
