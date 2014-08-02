0853a256ac562e0fdb830f9e848947f14bc5f067
Tom Stuart <tom@codon.com>
2014-08-02 13:52:17 +0200

    Implement Many#and_then

--- lib/monads/many.rb
+++ lib/monads/many.rb
@@ -1,3 +1,7 @@
 module Monads
-  Many = Struct.new(:values)
+  Many = Struct.new(:values) do
+    def and_then(&block)
+      Many.new(values.map(&block).flat_map(&:values))
+    end
+  end
 end
--- spec/monads/many_spec.rb
+++ spec/monads/many_spec.rb
@@ -2,12 +2,37 @@ require 'monads/many'
 
 module Monads
   RSpec.describe 'the Many monad' do
-    let(:values) { double }
     let(:many) { Many.new(values) }
 
     describe '#values' do
+      let(:values) { double }
+
       it 'retrieves the values from a Many' do
         expect(many.values).to eq values
+      end
+    end
+
+    describe '#and_then' do
+      context 'when there aren’t any values' do
+        let(:values) { [] }
+
+        it 'doesn’t call the block' do
+          expect { |block| many.and_then(&block) }.not_to yield_control
+        end
+      end
+
+      context 'when there are values' do
+        let(:values) { [1, 2, 3] }
+
+        it 'calls the block with each value' do
+          @values = []
+          many.and_then { |value| @values << value; Many.new(double) }
+          expect(@values).to eq values
+        end
+
+        it 'returns a flattened version of the block’s results' do
+          expect(many.and_then { |value| Many.new(value * 2) }.values).to eq [2, 4, 6]
+        end
       end
     end
   end
